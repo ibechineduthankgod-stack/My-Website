5e4d5f0fa16b88c1a199a5dae864d90bc0032f0e
ibechineduthankgod-stack <ibechineduthankgod@gmail.com>
2026-03-28 14:49:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -102,3 +102,36 @@ menuToggle.onclick = ()=>{
 navLinks.classList.toggle("active");
 }
 }
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+counter.innerText = "0";
+
+const updateCounter = () => {
+
+const target = +counter.getAttribute("data-target");
+
+const count = +counter.innerText;
+
+const increment = target / 100;
+
+if(count < target){
+
+counter.innerText = Math.ceil(count + increment);
+
+setTimeout(updateCounter,20);
+
+}
+
+else{
+
+counter.innerText = target;
+
+}
+
+};
+
+updateCounter();
+
+});
